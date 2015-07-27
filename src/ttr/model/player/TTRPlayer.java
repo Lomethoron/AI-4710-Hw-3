fbfea4575b5637fac8cba4db0f8e57131578093e
Will Dixon <wdixon23192@gmail.com>
2015-07-27 00:20:19 -0400

    getting closer to getting shortest path, does not currently overwrite better found paths

--- src/ttr/model/player/TTRPlayer.java
+++ src/ttr/model/player/TTRPlayer.java
@@ -18,7 +18,7 @@ public class TTRPlayer extends Player {
 	
 	@Override
 	public void makeMove() {
-		ArrayList<ArrayList<Route>> foo = getPath(Destination.Seattle, Destination.Denver);
+		ArrayList<ArrayList<Route>> foo = getPath(Destination.Seattle, Destination.Boston);
 		for(ArrayList<Route> routes :foo){
 			System.out.print(routes.get(0).getDest1().name()+" ");
 		}
@@ -39,11 +39,14 @@ public class TTRPlayer extends Player {
 		Routes routes = Routes.getInstance();
 		PriorityQueue<PathNode> openList = new PriorityQueue<PathNode>(1, new PathNodeComparator());
 		ArrayList<PathNode> closedList = new ArrayList<PathNode>();
+		HashMap<Destination, Destination> previousCityFinder = new HashMap<Destination, Destination>();
 		
 		//init
 		for(Destination dest: routes.getNeighbors(from)){ 
 			Route toDest = routes.getRoutes(dest, from).get(0);
 			openList.add(new PathNode(dest, from, toDest.getCost()));
+			previousCityFinder.put(dest, from);
+			System.out.println(from+" links to " +dest);
 		}
 		
 		while(!openList.isEmpty()){
@@ -59,15 +62,15 @@ public class TTRPlayer extends Player {
 				pathBuilder.push(initRoute);
 				//build up the stack
 				while(pathNotComplete){
-					for(PathNode path: closedList){
-						if(path.getCurr() == prev){
-							pathBuilder.push(routes.getRoutes(path.getCurr(), path.getPrev()));
-							prev = path.getPrev();
-							if(prev == from){
-								pathNotComplete = false;
-							}
-						}
+					Destination next = previousCityFinder.get(prev);
+					System.out.println("Pushing "+ prev+ " to "+next);
+					pathBuilder.push(routes.getRoutes(prev, next));
+					if(next == from){
+						pathNotComplete = false;
+						continue;
 					}
+					prev = next;
+					
 				}
 				//reverse the ordering
 				ArrayList<ArrayList<Route>> finalRoute = new ArrayList<ArrayList<Route>>();
@@ -77,12 +80,22 @@ public class TTRPlayer extends Player {
 				return finalRoute;
 			}
 			//keep searching
-			for(Destination dest: routes.getNeighbors(nextNode.getCurr())){ 
-				boolean foo = !closedList.contains(nextNode);
+			for(Destination dest: routes.getNeighbors(nextNode.getCurr())){
+				int bestCost = routes.shortestPathcost(from, dest);
+				System.out.println("Best Cost: "+bestCost+ " Current Cost: "+nextNode.getDistToStart());
+				if(!previousCityFinder.containsKey(dest)&&dest!=from){
+					previousCityFinder.put(dest, nextNode.getCurr());
+					System.out.println(nextNode.getCurr()+" links to " +dest);
+				}
+				//System.out.println("Best Cost: "+bestCost+ " Current Cost: "+nextNode.getDistToStart());
+				else if(previousCityFinder.containsKey(dest)&&bestCost<nextNode.getDistToStart()){
+					previousCityFinder.put(dest, nextNode.getCurr());
+					System.out.println(nextNode.getCurr()+" links to " +dest);
+				}
 				if(!closedList.contains(nextNode)){
 					Route toDest = routes.getRoutes(nextNode.getCurr(), dest).get(0);
-					openList.add(new PathNode(dest, nextNode.getCurr(), toDest.getCost()));
-					System.out.println("Adding "+nextNode.getCurr().name()+" to "+ dest.name());
+					openList.add(new PathNode(dest, nextNode.getCurr(), toDest.getCost()+nextNode.getDistToStart()));
+					//System.out.println("Adding "+nextNode.getCurr().name()+" to "+ dest.name());
 				}
 			}
 			
